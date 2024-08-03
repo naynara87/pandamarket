8c0247e3ddd21b01ee2b15088a857dd37daca01a
nana <nay1121@naver.com>
2024-08-03 21:14:01 +0900

    refactor: 최적화

--- src/components/form/TagInput.js
+++ src/components/form/TagInput.js
@@ -1,34 +1,38 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
-const TagInput = ({ onTagListChange, reset }) => {
-  const [tagInputValue, setTagInputValue] = useState('');
+const TagInput = ({ label, onTagListChange, reset }) => {
+  const [tagInputValue, setTagInputValue] = useState("");
   const [tagList, setTagList] = useState([]);
 
   const handleKeyPress = useCallback(
-    e => {
-      if (e.key === 'Enter' && tagInputValue.trim()) {
+    (e) => {
+      if (e.key === "Enter" && tagInputValue.trim()) {
         e.preventDefault();
-
+        const newTagName = tagInputValue.trim();
         const newTag = {
-          id: Date.now(), // 고유 ID 생성 (현재 시간 기반)
-          name: tagInputValue.trim(),
+          id: Date.now(),
+          name: newTagName,
         };
-
         const newTagList = [...tagList, newTag];
         setTagList(newTagList);
-        setTagInputValue('');
         onTagListChange(newTagList);
+        setTagInputValue("");
       }
     },
-    [tagInputValue, tagList, onTagListChange],
+    [tagInputValue, tagList, onTagListChange]
   );
+
   const handleTagRemove = useCallback(
-    tagIdToRemove => {
-      const updatedList = tagList.filter(tag => tag.id !== tagIdToRemove);
-      setTagList(updatedList);
-      onTagListChange(updatedList);
+    (tagIdToRemove) => {
+      setTagList((prevTagList) => {
+        const updatedList = prevTagList.filter(
+          (tag) => tag.id !== tagIdToRemove
+        );
+        onTagListChange(updatedList);
+        return updatedList;
+      });
     },
-    [tagList, onTagListChange],
+    [onTagListChange]
   );
 
   useEffect(() => {
@@ -39,24 +43,30 @@ const TagInput = ({ onTagListChange, reset }) => {
   }, [reset, onTagListChange]);
 
   return (
-    <div className="tag-input-container">
-      <input
-        type="text"
-        name="tag"
-        placeholder="태그를 입력 후 Enter를 눌러 추가하세요"
-        value={tagInputValue}
-        onChange={e => setTagInputValue(e.target.value)}
-        onKeyUp={handleKeyPress}
-      />
-      {tagList.length > 0 && (
-        <div className="tag-list-input">
-          {tagList.map(tag => (
-            <span key={tag.id}>
-              {tag.name} <i className="icon ic_remove" onClick={() => handleTagRemove(tag.id)}></i>
-            </span>
-          ))}
-        </div>
-      )}
+    <div className="input-group">
+      <label>{label}</label>
+      <div className="tag-input-container">
+        <input
+          type="text"
+          placeholder="태그를 입력 후 Enter를 눌러 추가하세요"
+          value={tagInputValue}
+          onChange={(e) => setTagInputValue(e.target.value)}
+          onKeyUp={handleKeyPress}
+        />
+        {tagList.length > 0 && (
+          <div className="tag-list-input">
+            {tagList.map((tag) => (
+              <span key={tag.id}>
+                {tag.name}{" "}
+                <i
+                  className="icon ic_remove"
+                  onClick={() => handleTagRemove(tag.id)}
+                ></i>
+              </span>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
